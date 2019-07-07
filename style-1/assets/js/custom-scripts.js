bcc00696f18729b219ada34105ddab7b142da36d
Siful <sifulsmm@gmail.com>
2019-07-07 18:09:33 +0600

    portfolio pages upadte

--- style-1/assets/js/custom-scripts.js
+++ style-1/assets/js/custom-scripts.js
@@ -4,6 +4,35 @@
     $.fn.andSelf = function() {
       return this.addBack.apply(this, arguments);
     }
+
+
+    /* Loader Code Start */
+    $(window).on("load", function() { 
+        $(".section-loader").fadeOut("slow");
+        
+        var $container = $('.portfolioContainer');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                queue: true
+            }
+        });
+     
+        $('.portfolio-nav li').click(function(){
+            $('.portfolio-nav .current').removeClass('current');
+            $(this).addClass('current');
+     
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    queue: true
+                }
+             });
+             return false;
+        });
+      });
+    /* Loader Code End */
     
     
      /* 
